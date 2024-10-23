ce05314337420b44dcc249a550b25cac5b07a83e
Extager <164411975+Extager@users.noreply.github.com>
2024-10-23 16:10:44 +0300

    block-1

--- src/components/comp_1/comp_1.jsx
+++ src/components/comp_1/comp_1.jsx
@@ -1,12 +1,21 @@
-import './com_1.css'
+import './com_1.css';
+import Japan from '../../images/Japan.jpg';
+
 export default function Comp_1(){
     return(
         <div className="comp_1">
-            <p>Text1</p>
-            <p>Автостопом по Японии</p>
-            <button>
-                <span>Поехали</span>
-            </button>
+            <img src={Japan} alt="Connection Error" id='image' />
+            <div className="text-btn">
+                <div className='text'>
+                    <span id='text_1'> МЕРЕДИАН ТУР</span>
+                    <p id='text_2'>Автостопом по Японии</p>
+                </div>
+                <div className='btn'>
+                    <button id='btn-go'>
+                        <span>Поехали</span>
+                    </button>
+                </div>
+            </div>
         </div>
     );
 }
